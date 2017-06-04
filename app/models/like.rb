e606b0615fa2332a880dfe2cef3898e2b9e37b9e
rails -v <dirk.pearson@yhlsoft.com>
2017-06-04 08:57:18 -0500

    80%

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,6 @@ class Like < ApplicationRecord
 validates :photo_id, :uniqueness => {:scope => :user_id, :message => "already bookmarked"}
 
 belongs_to :photo
+belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
          has_many :photos
          has_many :likes
+
+         has_many :liked_photos, :through => :likes, :source => :photo
+         
 end
